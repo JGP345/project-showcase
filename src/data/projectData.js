8e09c78a36756e4d87222c0659142e2d4f0be7be
JGP345 <88603051+JGP345@users.noreply.github.com>
2025-08-21 18:15:27 -0500

    Update project data with new project details

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,18 +1,18 @@
 // src/data/projectData.js
 export const projects = [
-  {
+    {
     id: 1,
     title: {
-      en: "",
-      es: "",
+      en: "Home Builders",
+      es: "Constructores de Hogar",
     },
     description: {
-      en: "",
-      es: "",
+      en: "A project showcasing home building techniques.",
+      es: "Un proyecto que muestra técnicas de construcción de viviendas.",
     },
     image: "",
-    liveSite: "",
-    githubRepo: "",
+    liveSite: "https://jgp345.github.io/HomeBuilders4/#",
+    githubRepo: "https://github.com/JGP345/HomeBuilders4",
   },
   // Add more projects here...
 ];
