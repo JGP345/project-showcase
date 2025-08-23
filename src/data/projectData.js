6d6539b5b2650d5f0a33926490afa633b1d78f60
JGP345 <88603051+JGP345@users.noreply.github.com>
2025-08-23 09:38:43 -0500

    Agrege mi website

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,16 +3,16 @@ export const projects = [
     {
     id: 1,
     title: {
-      en: "Home Builders",
-      es: "Constructores de Hogar",
+      en: "Homebuilder",
+      es: "Case Constructores",
     },
     description: {
-      en: "A project showcasing home building techniques.",
-      es: "Un proyecto que muestra técnicas de construcción de viviendas.",
+      en: "This is HomeBuilders a website dedicated to building omes for the homeless with love.",
+      es: "Esto es HomeBuilders un website dedicado a contruir casas para la personas sin casa con amor.",
     },
     image: "",
-    liveSite: "https://jgp345.github.io/HomeBuilders4/#",
-    githubRepo: "https://github.com/JGP345/HomeBuilders4",
+    liveSite: "https://jgp345.github.io/HomeBuilders2/",
+    githubRepo: "https://github.com/JGP345/HomeBuilders2",
   },
   // Add more projects here...
 ];
